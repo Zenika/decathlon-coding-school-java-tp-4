7ff68a61ce1587d2c612a83d951b7db8b1dd507b
fcorni25 <francoise.cornille@decathlon.com>
2019-03-19 15:11:10 +0100

    Commit TP4

--- src/main/java/com/zenika/decathlon/tp4/visibilite/package1/ClassB.java
+++ src/main/java/com/zenika/decathlon/tp4/visibilite/package1/ClassB.java
@@ -2,5 +2,15 @@ package com.zenika.decathlon.tp4.visibilite.package1;
 
 public class ClassB {
 	// TODO tester ici les différents champs de la class A ... Mais comment ?
+	
+	
+	
+	public void test() {
+		ClassA a = new ClassA();
+		a.attributPackage.equals(a); 
+		a.attributProtected.endsWith(null);
+		a.attributPublic.isEmpty();
+		
+	}
 
 }
